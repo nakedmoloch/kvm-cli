f5df80b25cbfd39768c20d2217f47c8d15bc85d6
nakedmoloch <153424377+nakedmoloch@users.noreply.github.com>
2023-12-10 18:34:52 +0000

    Update kvm_vm_script.sh

--- kvm_vm_script.sh
+++ kvm_vm_script.sh
@@ -12,7 +12,7 @@ echo "Network in bridge (br0) mode by default"
 read -p "Do you have an ISO file for installation? (Y/n): " has_iso
 
 if [[ $has_iso =~ ^[Yy]$ || -z $has_iso ]]; then
-    read -p "Enter the absolute path where the ISO files are located (omit the las '/'): " iso_directory
+    read -p "Enter the absolute path where the ISO files are located: " iso_directory
 
     # List available .iso files in the specified directory
     iso_files=("$iso_directory"/*.iso)
@@ -23,30 +23,30 @@ if [[ $has_iso =~ ^[Yy]$ || -z $has_iso ]]; then
     fi
 
     echo "Available .iso files:"
-    ls "$iso_directory" | grep *.iso
+    ls "$iso_directory"/*.iso
 
     # Prompt user to choose an .iso file
     read -p "Enter the name of the desired .iso file: " iso_file
     iso_path="$iso_directory/$iso_file"
-    cdrom_option="--cdrom $iso_path"
+    cdrom_option="--cdrom=$iso_path"
     location_option=""
 else
     read -p "Enter the URL of the amd file: " iso_url
     cdrom_option=""
-    location_option="--location '$iso_url'"
+    location_option="--location='$iso_url'"
 fi
 
 # Run virt-install with the specified options
 virt-install \
---name $vm_name \
---disk path=$disk_image/$vm_name.qcow2,size=$disk_size \
---vcpus $vcpus_value \
---ram $ram_value \
---os-variant $os_value \
---network bridge=br0 \
---graphics spice \
---video qxl \
---channel spicevmc \
---console pty,target_type=serial \
-$cdrom_option \ 
-$location_option
+    --name $vm_name \
+    --memory $ram_value \
+    --vcpus $vcpus_value \
+    --os-variant $os_value \
+    --disk path=$disk_image/$vm_name.qcow2,size=$disk_size \
+    --network bridge=br0 \
+    --graphics spice \
+    --video qxl \
+    --channel spicevmc \
+    --console pty,target_type=serial \
+    $cdrom_option \
+    $location_option
